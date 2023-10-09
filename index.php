70f3a2a24d8dfdedde6929c4fd5aa73246b4df84
EliaBuratti <elia.buratti.dev@gmail.com>
2023-10-09 14:58:11 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad words</title>
+</head>
+
+<body>
+
+    <form action="script.php" method="get">
+        <input type="text" name="paragraph" id="paragraph" placeholder="Type a phrase">
+        <input type="text" name="wordBan" id="wordBan" placeholder="Type word to be banned">
+        <button type="submit">Censura</button>
+    </form>
+
+</body>
+
+</html>
