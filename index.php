7b85a1603d6886422f90ea9224ddf8ce5ddc1b0c
EliaBuratti <elia.buratti.dev@gmail.com>
2023-10-09 15:54:33 +0200

    add input field and result filtered

--- index.php
+++ index.php
@@ -7,13 +7,37 @@
     <title>Bad words</title>
 </head>
 
+<style>
+    body {
+        width: 80%;
+        height: 80%;
+        margin: 2rem auto;
+        text-align: center;
+        font-family: sans-serif;
+    }
+
+    section {
+        border: 2px solid lightgray;
+        border-radius: 1rem;
+        margin-bottom: 4rem;
+        padding: 1rem;
+    }
+</style>
+
 <body>
 
-    <form action="script.php" method="get">
-        <input type="text" name="paragraph" id="paragraph" placeholder="Type a phrase">
-        <input type="text" name="wordBan" id="wordBan" placeholder="Type word to be banned">
-        <button type="submit">Censura</button>
-    </form>
+    <section>
+
+        <h1>Write a phrase and type a word to be banned.</h1>
+        <h3> <mark>Warning: it's case sensitive!!</mark> </h3>
+
+        <form action="script.php" method="post">
+            <input type="text" name="paragraph" id="paragraph" placeholder="Type a phrase" required>
+            <input type="text" name="wordBan" id="wordBan" placeholder="Type word to be banned" required>
+            <button type="submit">Censura</button>
+        </form>
+
+    </section>
 
 </body>
 
--- script.php
+++ script.php
@@ -1,1 +1,63 @@
 <?php
+
+
+$paragraph = $_POST['paragraph'];
+$wordBan = $_POST['wordBan'];
+$paragraphBan = str_replace($wordBan, '***', $paragraph);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paragraph Ban</title>
+</head>
+
+<style>
+    body {
+        width: 80%;
+        height: 80%;
+        margin: 2rem auto;
+        text-align: center;
+    }
+
+    section {
+        border: 2px solid lightgray;
+        border-radius: 1rem;
+        margin-bottom: 4rem;
+        padding: 1rem;
+    }
+</style>
+
+<body>
+
+
+    <section id="input-user">
+        <h3> Paragrafo: </h3>
+        <p>
+            <?php echo $paragraph ?>
+        </p>
+        <h3> Lunghezza: <?php echo strlen($paragraph) ?></h3>
+    </section>
+
+    <section id="paragraph-word-banned">
+        <h2> Parola da bannare: <?php echo $wordBan ?> </h2>
+
+        <h3> Paragrafo con parole bannate: </h3>
+        <p>
+            <?php echo $paragraphBan ?>
+        </p>
+        <h3> Lunghezza: <?php echo strlen($paragraphBan) ?></h3>
+    </section>
+
+
+
+
+
+
+</body>
+
+</html>
